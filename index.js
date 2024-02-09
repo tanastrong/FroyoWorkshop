9e8a46ce777f540ec9b14bf3a38797f2ac8d6388
tanastrong <148703474+tanastrong@users.noreply.github.com>
2024-02-08 18:52:18 -0800

    Update index.js
    corrected loop...i hope

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-
 const froyoOrder = prompt("Please enter you fryoy order, Seperate each order with a comma!")
 
 const froyoArray = froyoOrder.split(",")
 
-console.log(froyoOrder.split(","))
-
-const froyoObject = {}
-
-for(let i=0; i<froyoArray.length;i++){const froyoObject = {}
-    if(froyoObject[froyoArray[i]]){
-        
-        froyoObject[froyoArray[i]] = froyoObject[froyoArray[i]] +1
-
-    }else{
-        froyoObject[froyoArray[i]] = 1
-     }
-}
-console.table(froyoObject)  
-
+const froyoFunction = (arr) => {
+    const froyoObject = {};
+    for (let i=0; i < arr.length; i++){
+        if(froyoObject[arr[i]]){
+            froyoObject[arr[i]] =  froyoObject[arr[i]]+1
+        }else{
+            froyoObject[arr[i]] = 1;
+        }
+    }
+    return froyoObject;
+};
+console.table(froyoFunction(froyoArray))
